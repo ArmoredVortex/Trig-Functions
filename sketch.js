9294bef6c1ad671ebc639906e7032b0492a3d2f5
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2021-09-02 12:32:51 +0530

    Added cosine wave

--- sketch.js
+++ sketch.js
@@ -2,17 +2,23 @@ var center;
 let constant = 120;
 let angle = 0.05;
 let scalar = 100;
-let speed = 0.05;
+let speed = 0.03;
 let sine = [];
-let angles = [];
-let angleV = [];
+let sineAngles = [];
+let sineAngleV = [];
+let cosAngles = [];
+let cosAngleV = [];
 let r = 4;
 function setup() {
   createCanvas(windowWidth , windowHeight);
   center = 120
   let total = floor(width / (r * 2));
   for (let i = 0; i < total + 1; i++) {
-    angles[i] = map(i, 0, total, 0, 5 * TWO_PI);
+    sineAngles[i] = map(i, 0, total, 0, 2 * TWO_PI);
+    // angleV[i] = 0.01 + i / 100;
+  }
+  for (let i = 0; i < total + 1; i++) {
+    cosAngles[i] = map(i, 0, total, 0, 2 * TWO_PI);
     // angleV[i] = 0.01 + i / 100;
   }
 }
@@ -20,6 +26,7 @@ function setup() {
 function draw() {
   background(100);
   fill(100)
+  stroke(2);
   ellipse(center,center,200)
   let x = constant + sin(angle) * scalar;
   let y = constant + cos(angle) * scalar;
@@ -35,14 +42,28 @@ function draw() {
 //     sine.pop();
 //   }
   beginShape();
-  for (let i = 0; i < angles.length; i++) {
-    let y = map(cos(angles[i]), -1, 1, 20, 220);
-    strokeWeight(4);
-    let x = map(i, 0, angles.length, 300, 1000);
+  for (let i = 0; i < sineAngles.length; i++) {
+    let y = map(cos(sineAngles[i]), -1, 1, 20, 220);
+    noStroke();
+    let x = map(i, 0, sineAngles.length, 300, 1000);
     // line(x, 0, x, y);
+    fill(0)
     circle(x, y, r * 2);
     // vertex(x,y);
-    angles[i] += 0.05;
+    sineAngles[i] += 0.03;
+    // angles[i] += angleV[i];
+  }
+  endShape();
+  beginShape();
+  for (let i = 0; i < cosAngles.length; i++) {
+    let y = map(sin(cosAngles[i]), -1, 1, 20, 220);
+    let x = map(i, 0, cosAngles.length, 300, 1000);
+    // line(x, 0, x, y);
+    noStroke();
+    fill(200,0,0)
+    circle(x, y, r * 2);
+    // vertex(x,y);
+    cosAngles[i] += 0.03;
     // angles[i] += angleV[i];
   }
   endShape();
